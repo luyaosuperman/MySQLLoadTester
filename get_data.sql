7be2d86248fb3d41f5247eb90906d739533c49e9
luyaosuperman <luyaosuperman@gmail.com>
2016-06-10 20:02:09 +0800

    query update

--- get_data.sql
+++ get_data.sql
@@ -1,8 +1,20 @@
 select 
 a.systemNanoTime,
-a.totalExecutionCount,
- a.totalExecutionCount - @lasttotalExecutionCount as intervalExecutionCount,
-@lasttotalExecutionCount := a.totalExecutionCount
+@rowcount + a.insertCount as rowCount,
+a.runCount - @lasttotalrunCount as intervalrunCount,
+a.insertCount - @lasttotalinsertCount as intervalinsertCount,
+a.updateCount - @lasttotalupdateCount as intervalupdateCount,
+a.selectCount - @lasttotalselectCount as intervalselectCount,
+@lasttotalrunCount := a.runCount,
+@lasttotalinsertCount := a.insertCount,
+@lasttotalupdateCount := a.updateCount,
+@lasttotalselectCount := a.selectCount
 from testreport.testruntimeinfo a,
-(select @lasttotalExecutionCount := 0) SQLVars
-where testid = 7
+(select 
+@lasttotalrunCount := 0,
+@lasttotalinsertCount := 0,
+@lasttotalupdateCount := 0,
+@lasttotalselectCount := 0,
+@rowcount := initdataamount from testinfo where id = 22
+) SQLVars
+where testid = 22
