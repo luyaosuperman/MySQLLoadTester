422bff871cf3ebf1d9e071dd9e482003b4874e54
luyaosuperman <luyaosuperman@gmail.com>
2016-09-06 12:02:00 +0000

    one more commit

--- MySQLLoadTester/src/main/com/MysqlLoadTest/ExecutionUnit/HibernateVersion/HTester.java
+++ MySQLLoadTester/src/main/com/MysqlLoadTest/ExecutionUnit/HibernateVersion/HTester.java
@@ -1,9 +1,18 @@
 package com.MysqlLoadTest.ExecutionUnit.HibernateVersion;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 
 import com.MysqlLoadTest.Interfaces.TestConfig;
 import com.MysqlLoadTest.Interfaces.TestController;
@@ -16,8 +25,27 @@ public class HTester {
 
 	public static void main(String[] args){
 		
-		//TestController testController = new HTestController();
-		//TestController testController = new HTestController();
+		List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
+		classLoadersList.add(ClasspathHelper.contextClassLoader());
+		classLoadersList.add(ClasspathHelper.staticClassLoader());
+
+		Reflections reflections = new Reflections(new ConfigurationBuilder()
+		    .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
+		    .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+		    .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("com.MysqlLoadTest.ExecutionUnit.HibernateVersion"))));
+		
+		Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
+		
+		log.info("allClasses.size(): " +allClasses.size());
+		for(Class<?> c: allClasses){
+				log.info(c.isAssignableFrom(com.MysqlLoadTest.Interfaces.TestController.class) + " " + c.getName());
+		}
+		
+		Set<String> classes= reflections.getAllTypes();
+		classes.forEach(s -> log.info(s));
+		
+		/*
+		
 		TestController testController = null;
 		try {
 			testController = (TestController) Class.forName("com.MysqlLoadTest.ExecutionUnit.HibernateVersion.HTestController").newInstance();
@@ -36,6 +64,8 @@ public class HTester {
 		
 		
 		testController.startTest();
+		
+		*/
 		
 	}
 	
